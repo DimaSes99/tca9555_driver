2287886828b63b70d2786c52193e87fbb8d93347
Alexander Grin <alexander.grin.dev@gmail.com>
2020-10-18 17:08:41 +0300

    Rename byte swapping macro

--- tca9555_driver.c
+++ tca9555_driver.c
@@ -36,8 +36,8 @@
 
 #if ((BIG_ENDIAN) || (defined __C51__) || (defined __RC51__) || (defined _CC51))
 //! Big Endian Compilers: Bit swapping necessary
-#define htobe16(x) ((((x) >> 8) & 0x00FF) | (((x) << 8) & 0xFF00))
-#define be16toh(x) ((((x) >> 8) & 0x00FF) | (((x) << 8) & 0xFF00))
+#define htole16(x) ((((x) >> 8) & 0x00FF) | (((x) << 8) & 0xFF00))
+#define le16toh(x) ((((x) >> 8) & 0x00FF) | (((x) << 8) & 0xFF00))
 #elif ((LITTLE_ENDIAN) || (defined __GNUC__) || (defined __clang__) ||(defined SDCC) || (defined HI_TECH_C) || (defined __ICC8051__))
 #define htole16(x) (x)
 #define le16toh(x) (x)
